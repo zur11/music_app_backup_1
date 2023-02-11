c0e0aeadf72752b0293d3bfe70047d349bb9ec62
fernandobarahona <33161338+fernandobarahona@users.noreply.github.com>
2023-02-11 10:26:13 -0500

    1drft getiing out history from scnmnger to hstmngr

--- global_systems/scene_manager_system/system/scene_manager.gd
+++ global_systems/scene_manager_system/system/scene_manager.gd
@@ -3,57 +3,76 @@ class_name SceneManager extends Node2D
 var _async_loader = AsyncLoader.new()
 var _container: Node2D
 
-var _previous_instanced_scene : Node
+var _history_manager:= HistoryManager.new()
 
-var _scene_history : Array
-
-var _theme = "theme_1"
+#var _scene_history : Array = []
+#var _previous_instanced_scene : Node
+#
+#var _theme = "theme_1"
 
 func _change_scene(instanced_scene_to_go: Node):
 	for child in _container.get_children():
 		self.remove_child(child)
 		await get_tree().process_frame
-		child.call_deferred("free")
+		child.free()
 	_container.add_child(instanced_scene_to_go)
 
 func goto_scene(instantiated_scene_to_go: Node):
-	_validate_scene_to_go(instantiated_scene_to_go)
+	if _validate_scene_to_go(instantiated_scene_to_go): return
+
+	_history_manager.add_new_scene(instantiated_scene_to_go)
+#	_scene_history.push_back(instantiated_scene_to_go.scene_file_path)
+#	_previous_instanced_scene = _get_previous_instance_scene() 
 
 	_change_scene(instantiated_scene_to_go)
-
-	_scene_history.push_back(instantiated_scene_to_go.scene_file_path)
-
-	_previous_instanced_scene = load(_scene_history[-2]).instantiate() if _scene_history.size() > 1 else null
 
 func _validate_scene_to_go(instantiated_scene_to_go: Node) -> bool:
 	if instantiated_scene_to_go == null: 
 		push_error("Trying to goto_scene to a Null Scene")
 		return true
-	if instantiated_scene_to_go.scene_file_path == _scene_history.back(): 
-		push_warning("Trying to goto_scene to A Scene same as Previous Scene")
+	if _history_manager._scene_history.size() > 0 and instantiated_scene_to_go.scene_file_path == _history_manager._scene_history.back(): 
+		#push_warning("Trying to goto_scene to A Scene same as Previous Scene")
 		return true
 	return false
 
 func goto_previous_scene():
-	if !_previous_instanced_scene : return
-	_change_scene(_previous_instanced_scene)
+	if !_history_manager._previous_instanced_scene : return
+	var instantiated_scene_to_go = _history_manager._previous_instanced_scene
 
-	_scene_history.pop_back()
-
-	_previous_instanced_scene = load(_scene_history[-2]).instantiate() if _scene_history.size() > 1 else null
+#	_scene_history.pop_back()
+#	_previous_instanced_scene = _get_previous_instance_scene() 
+	_history_manager.remove_last_scene()
+	
+	_change_scene(instantiated_scene_to_go)
 
 func update_theme(new_theme: String):
 
-	for ii in _scene_history.size():
-		_scene_history[ii] = _scene_history[ii].trim_suffix(_theme + ".tscn") + new_theme + ".tscn"
+#	for ii in _scene_history.size():
+#		_scene_history[ii] = _scene_history[ii].trim_suffix(_theme + ".tscn") + new_theme + ".tscn"
+#	_previous_instanced_scene = _get_previous_instance_scene() 
+#	_theme = new_theme
+	_history_manager.update_themes(new_theme)
 
-	_previous_instanced_scene = load(_scene_history[-2]).instantiate() if _scene_history.size() > 1 else null
+	self._change_scene(_history_manager._get_current_instantiated_scene())
 
-	_theme = new_theme
+#func _get_previous_instance_scene() -> Node:
+#	return load(_scene_history[-2]).instantiate() if _scene_history.size() > 1 else null
+#
+#func _get_current_instantiated_scene() -> Node:
+#	return load(_scene_history[-1]).instantiate()
 
-	self._change_scene(load(_scene_history[-1]).instantiate())
 
+
+
+
+
+
+
+
+
+#------------------------------
 #FUNCTIONS FOR THREADED LOADING
+#------------------------------
 func async_load(scene_urls: Array):
 	await _async_loader.load_scenes(scene_urls)
 
