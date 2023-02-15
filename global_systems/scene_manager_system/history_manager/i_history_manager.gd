332502e5161ec7bda41751d770715d0a3b15c6f4
fernandobarahona <33161338+fernandobarahona@users.noreply.github.com>
2023-02-15 17:01:06 -0500

    moved scene manager files

--- global_systems/scene_manager_system/history_manager/i_history_manager.gd
+++ global_systems/scene_manager_system/history_manager/i_history_manager.gd
@@ -1,11 +1,11 @@
 class_name IHistoryManager extends Resource
 
-func add_new_scene(instantiated_scene_to_go) -> void:
+func add_new_scene(_instantiated_scene_to_go) -> void:
 	pass
 
 func remove_last_scene() -> void:
 	pass
 
-func update_themes(new_theme) -> void:
+func update_themes(_new_theme) -> void:
 	pass
 
